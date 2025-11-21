7904cb54ce65e11ae32b5fc6c1895e1de2a6e525
Den <103452827+LourdenB15@users.noreply.github.com>
2025-11-21 22:53:17 +0800

    feat: add React Router with page components and route configuration

---
+++ web/src/pages/ConfirmationPage.jsx
@@ -0,0 +1,9 @@
+function ConfirmationPage() {
+  return (
+    <div>
+      <h1>Confirmation Page</h1>
+    </div>
+  )
+}
+
+export default ConfirmationPage
---
+++ web/src/pages/HotelDetailsPage.jsx
@@ -0,0 +1,9 @@
+function HotelDetailsPage() {
+  return (
+    <div>
+      <h1>Hotel Details Page</h1>
+    </div>
+  )
+}
+
+export default HotelDetailsPage
